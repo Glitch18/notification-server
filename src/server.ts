be271c80fa8f6e1fd5b933eb71250b1674b44006
Atharva <atharvakn@gmail.com>
2024-03-13 14:07:22 +0530

    feat: configure port using env var

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { getApiRouter } from "./api";
 import { bot } from "./bot";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 bot.launch(); // Start the bot
